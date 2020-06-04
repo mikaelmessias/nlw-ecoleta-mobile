e55ffcff0955f14d0f3c605b593b82aa0881d340
Mikael Messias <mikaelmessias@outlook.com>
2020-06-04 17:03:13 -0300

    Add router support and other dependencies
    Signed-off-by: Mikael Messias <mikaelmessias@outlook.com>

--- App.tsx
+++ App.tsx
@@ -4,6 +4,8 @@ import { StatusBar } from 'react-native';
 import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
 import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu';
 
+import Routes from './src/routes';
+
 export default function App() {
   const [fontsLoaded] = useFonts({
     Roboto_400Regular,
@@ -18,6 +20,7 @@ export default function App() {
   return (
     <>
       <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent />
+      <Routes />
     </>
   );
 }
